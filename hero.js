8d001b3f64db0128bb9adb9cd2dd81ca4ab52883
Steven Wendryll <stevenwendryll@hotmail.com>
2024-01-23 21:04:28 -0300

    segundo commit

--- hero.js
+++ hero.js
@@ -1,1 +1,32 @@
-var heroi = "Jin woo"
+let heroi = "Raze"
+let xp = 5000
+let rank = ""
+const texto = `O Herói de nome ${heroi} está no nível de: `
+
+if (xp < 1000){
+    rank = "Ferro"
+}
+ else if (xp <= 2000){
+    rank = "Bronze"
+ }
+ else if (xp <= 5000){
+    rank = "Prata"
+ }
+ else if (xp <= 7000){
+    rank = "Ouro"
+ }
+ else if (xp <= 8000){
+    rank = "Platina"
+ }
+ else if (xp <= 9000){
+    rank = "Ascendente"
+ }
+ else if (xp <= 10000){
+    rank = "Imortal"
+ }
+ else {
+    (xp > 10000)
+    rank = "Radiante"
+ }
+
+ console.log (texto + rank)
